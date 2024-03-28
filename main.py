a7b133e662e99275d843e648b433aa713b3b8c23
Tiana <91290176+TianaKLarmond@users.noreply.github.com>
2024-03-27 23:59:09 -0500

    Update main.py
    Combined changes to http _flood with rate_limit code

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import threading
 import subprocess
 import random
 import urllib3
+from datetime import datetime, timedelta
+import time
 
 urllib3.disable_warnings()
 
-# fake IP address, but this doesn't make us anonymous
+# Fake IP address, but this doesn't make us anonymous
 fake_ip = '192.168.1.100'
+
+# Global variables to track rate limiting
+last_request_time = datetime.min
+lock = threading.Lock()
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 5.1; U; en) Opera 8.0",
@@ -24,11 +30,30 @@ user_agents = [
 
 def show_logs():
     try:
-        log_display = subprocess.check_output(['tail', '-n', '10', '/var/lo0g/apache2/access.log'])
+        log_display = subprocess.check_output(['tail', '-n', '10', '/var/log/apache2/access.log'])
         log_display = log_display.decode('utf-8')
         print(log_display)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
+
+
+def rate_limiting():
+    global last_request_time, lock
+
+    print("Executing rate limiting (1 second between allowed requests)...")
+
+    # Acquire lock to ensure thread safety
+    with lock:
+        current_time = datetime.now()
+        time_since_last_request = current_time - last_request_time
+
+        # Checks if the rate limit has been reached
+        if time_since_last_request < timedelta(seconds=1):
+            # Sleep to enforce rate limiting
+            time.sleep((timedelta(seconds=1) - time_since_last_request).total_seconds())
+
+        # Update last request time
+        last_request_time = datetime.now()
 
 def http_flood(target_url, num_of_requests):
     # Flood the webpage with requests
